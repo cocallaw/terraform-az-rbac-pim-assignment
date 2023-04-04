7bb14c82aa2c5ba114f1afd1406c6e5add7af597
Corey Callaway <coreylcallaway@gmail.com>
2023-04-04 18:27:40 -0400

    Update TF Azure Provider Versions

--- PIM Assignment - Resource Group/main.tf
+++ PIM Assignment - Resource Group/main.tf
@@ -29,7 +29,7 @@ terraform {
     required_providers {
         azurerm = {
             source  = "hashicorp/azurerm"
-            version = "~> 2"
+            version = "~> 3"
         }
     }
 }
--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
     required_providers {
         azurerm = {
             source  = "hashicorp/azurerm"
-            version = "~> 2"
+            version = "~> 3"
         }
         azuread = {
             source  = "hashicorp/azuread"
